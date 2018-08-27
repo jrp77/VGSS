8e4a6868bfa6cd3c7b5ccc8147c7d67845e5a7a8
jrp77 <hopperpo@gmail.com>
2018-08-26 23:43:31 -0500

    Added PlayerAttack.cs, doesn't work at all yet
    Started working on player combat. I've been trying to do it all through scripting but the more and more I work on this script, the more and more I'm starting to realize that I'm probably going to have to do animation to make this work.

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour 
+{
+	[Header("Basic Stuff")]
+	public Transform weapon;
+	public float attackTime;
+	public int weaponDamage;
+	public bool attacking;
+	public Transform finalAttackPosition;
+	private float _rememberX;
+	private float _rememberY;
+	private MeshRenderer _mesh;
+
+	[Header("Keys")]
+	public KeyCode attackKey;
+
+	void Start ()
+	{
+		_mesh = gameObject.GetComponent<MeshRenderer>();
+		_mesh.enabled = false;
+	}
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(attackKey) && !attacking)
+		{
+			attacking = true;
+			StartCoroutine("Attack");
+		}
+	}
+
+	IEnumerator Attack ()
+	{
+		//enable mesh renderer
+		_rememberX = transform.position.x;
+		_rememberY = transform.position.y;
+
+		_mesh.enabled = true;
+
+		//go from right to left in certain amount of time
+
+		float t = 0f;
+		t += Time.deltaTime / attackTime;
+		transform.position = Vector3.Lerp(transform.position, finalAttackPosition.position, t);
+
+		//when it gets to the other end, turn off mesh renderer/reset position/set "attacking" to false
+
+		yield return new WaitForSeconds(t);
+		_mesh.enabled = false;
+		transform.Translate(finalAttackPosition.position.x - _rememberX, finalAttackPosition.position.y - _rememberY, 0f);
+		attacking = false;
+	}
+}
